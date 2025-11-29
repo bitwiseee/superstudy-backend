5f0518630392043b4344e12edb0b3ef460215d82
pro-coder520 <iremideadeyanju9@gmail.com>
2025-11-29 06:25:28 +0100

    Made some more changes

--- superstudy/settings.py
+++ superstudy/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'superstudy-backend-2.onrender.com']
 
 
 # Application definition
